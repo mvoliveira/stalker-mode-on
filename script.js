c69d063ea8eff3b809b01a3c94b87573fe138753
Henrique <habyte@gmail.com>
2017-08-25 11:08:06 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function stalkerModeOn() {
+  const linkedIn = document.body;
+  const annoyingModal = document.getElementById('advocate-modal');
+  annoyingModal.style.display = 'none';
+  linkedIn.style.overflowY = 'auto';
+}
+
+stalkerModeOn();
